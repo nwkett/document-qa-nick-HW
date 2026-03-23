2bfbc6846a08374fd5d31ea6be2bbad022f54056
nwkett <113996906+nwkett@users.noreply.github.com>
2026-03-23 02:07:06 +0000

    fix

--- HW/HW7.py
+++ HW/HW7.py
@@ -6,7 +6,7 @@ import anthropic
 
 @st.cache_data
 def load_articles():
-    return pd.read_csv("news.CSV").fillna("")
+    return pd.read_csv("news.csv").fillna("")
 
 @st.cache_resource
 def load_vector_db(_df):
